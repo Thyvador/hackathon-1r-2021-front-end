8f47819b80cc1bc9ee2ad4436a661e697be79c5a
Thyvador <alex.hiltcher@gmail.com>
2021-09-10 19:08:09 +0200

    setup qr code scanner

--- src/pages/QrCodeScannerPage.jsx
+++ src/pages/QrCodeScannerPage.jsx
@@ -1,14 +1,43 @@
+import { makeStyles } from "@material-ui/core";
 import Page from "component/Page";
-import { useState } from "react";
-import QrReader from "react-qr-scanner";
+import { useEffect, useRef, useState } from "react";
+import QrReader from "react-qr-reader";
+
+const useStyles = makeStyles({
+  scannerContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+});
 
 const QrCodeScannerPage = () => {
+  const classes = useStyles();
+  const scannerRef = useRef(null);
+
   const [result, setResult] = useState("NO result");
+  const [legacyMode, setLegacyMode] = useState(false);
+
+  const [previewStyle, setPreviewStyle] = useState({
+    width: 0,
+    height: 0,
+  });
+
   const delay = 100;
-  const previewStyle = {
-    height: 240,
-    width: 320,
-  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const pageContainer = document.getElementById("page-container");
+
+      console.log(pageContainer.offsetWidth);
+      setPreviewStyle({
+        width: pageContainer.offsetWidth * 0.8,
+        height: pageContainer.offsetWidth * 1.3 * 0.8,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleScan = (result) => {
     if (result) {
@@ -16,18 +45,33 @@ const QrCodeScannerPage = () => {
     }
   };
 
-  const handleError = (mode) => {
+  const handleError = (err) => {
+    console.error(err);
+    setLegacyMode(true);
+  };
+
+  const onScannerLoad = (mode) => {
     console.log(mode);
+  };
+
+  const openImageDialog = () => {
+    scannerRef.current.openImageDialog();
   };
 
   return (
     <Page title="Qr Code Scanner">
-      <QrReader
-        delay={delay}
-        style={previewStyle}
-        onError={handleError}
-        onScan={handleScan}
-      />
+      <div className={classes.scannerContainer}>
+        <QrReader
+          ref={scannerRef}
+          delay={delay}
+          style={previewStyle}
+          onLoad={onScannerLoad}
+          onError={handleError}
+          onScan={handleScan}
+          legacyMode={legacyMode}
+        />
+      </div>
+      <input type="button" value="Submit QR Code" onClick={openImageDialog} />
       <p>{result}</p>
     </Page>
   );
