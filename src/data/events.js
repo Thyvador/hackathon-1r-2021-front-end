5e8d226d9bedbd871804cd9e33c9f8935adde7f9
Thomas Moreau <tmoreau@supralog.com>
2021-09-12 01:37:55 +0200

    wip send events

--- src/data/events.js
+++ src/data/events.js
@@ -1,34 +1,49 @@
 import moment from "moment";
 
 const events = () => [
-  {
-    linkedObject: null,
-    location: {
-      id: "https://api.onerecord.fr/locations/bru-whs-2",
-      geolocation: {
-        id: "https://api.onerecord.fr/locations/bru-whs-2/geolocation",
-        latitude: 50.90363,
-        longitude: 4.45454,
-      },
-      code: "BRU", 
-      locationName: "Brussels Warehouse 2",
-      locationType: "Warehouse",
+  [{
+    "linkedObject": {
+        "id": "https://api.onerecord.fr/companies/msc/piece-dgs/container1"
     },
-    performedBy: {
-      id: "https://api.onerecord.fr/companies/swissport",
-      branch: {
-        location: {
-          id: "https://api.onerecord.fr/locations/bru",
-        },
-        branchName: "SWISSPORT Belgium",
-      },
-      iataCargoAgentCode: "SWISSPORT",
+    "location": {
+        "id": "https://api.onerecord.fr/locations/sha-whs"
     },
-    dateTime: moment(1628641338000).add(1, "hour").valueOf(),
-    eventCode: "LOAD",
-    eventName: "Container loaded at Brussels Warehouse 2",
-    eventTypeIndicator: "Actual",
-  },
+    "performedBy": {
+        "id": "https://api.onerecord.fr/companies/agility"
+    },
+    "dateTime": 1628813695000,
+    "eventCode": "LOAD",
+    "eventName": "(SCAN) Load container on the truck",
+    "eventTypeIndicator": "Actual"
+  }, {
+    "linkedObject": {
+        "id": "https://api.onerecord.fr/companies/asus/piece-dgs/pallet1"
+    },
+    "location": {
+        "id": "https://api.onerecord.fr/locations/sha-whs"
+    },
+    "performedBy": {
+        "id": "https://api.onerecord.fr/companies/agility"
+    },
+    "dateTime": 1628813695000,
+    "eventCode": "LOAD",
+    "eventName": "(SCAN) Load container on the truck",
+    "eventTypeIndicator": "Actual"
+  }, {
+    "linkedObject": {
+        "id": "https://api.onerecord.fr/companies/asus/items/zenbook1"
+    },
+    "location": {
+        "id": "https://api.onerecord.fr/locations/sha-whs"
+    },
+    "performedBy": {
+        "id": "https://api.onerecord.fr/companies/agility"
+    },
+    "dateTime": 1628813695000,
+    "eventCode": "LOAD",
+    "eventName": "(SCAN) Load container on the truck",
+    "eventTypeIndicator": "Actual"
+  }],
 ];
 
 export default events;
